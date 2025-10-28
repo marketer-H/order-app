b7ddf1314c2e1aff18ab14609d6be6493b647106
marketer-H <kjhwjdgk98@gmail.com>
2025-10-28 23:55:23 +0900

    cors 처리 코드 수정

--- server/server.js
+++ server/server.js
@@ -15,7 +15,6 @@ if (process.env.NODE_ENV === 'production' && process.env.ALLOWED_ORIGIN) {
   }
 }
 app.use(cors(corsOptions))
-app.options('*', cors(corsOptions))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
